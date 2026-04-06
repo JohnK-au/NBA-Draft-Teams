658bed2b730ce9428faf40122d1299d5cc35abc5
JohnK-au <john.t.kerr14@gmail.com>
2026-04-06 17:43:41 +1000

    fix: made the UI for current season and all-time more clear

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import { Dashboard } from "./pages/Dashboard";
 import "./styles.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import { AllTime } from "./pages/AllTime";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Season</Link>
-        <Link to="/alltime">All-Time</Link>
+      <nav className="navbar">
+        <NavLink to="/" className="navbar__link">Current Season</NavLink>
+        <NavLink to="/alltime" className="navbar__link">All-Time</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<Dashboard />} />
